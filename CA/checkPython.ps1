04002f058e5c58ad0d2efd742ad8614688b979a1
Uday Kiran Reddy <ukreddy@erwin.com>
2021-08-21 14:48:18 +0000

    prereqs
    prereqs for checkPython and Python

--- CA/checkPython.ps1
+++ CA/checkPython.ps1
@@ -4,24 +4,33 @@ function WriteLog($sMsg)
 	$sMsg | Out-File $tmp -Append
 }
 
-Set-Property -Name PYTHON_REQUIRED -Value 1
+#Set-Property -Name PYTHON_REQUIRED -Value 1
+$PYTHON_REQUIRED=$true
 $pythonVersion = $(python --version)
 
 if ($pythonVersion -eq $null)
 {
   WriteLog "Python not installed"
-  Exit
+  #$true | Out-File $($($env:windir)+"\temp\PYTHON_REQUIRED.txt")
+  #Exit
 }
 elseif($pythonVersion.startswith('Python')){
   $pythonVersionNumber = $pythonVersion -replace "^\w*\s"
   if ($pythonVersionNumber -ne $null){
   if([System.Version]$pythonVersionNumber -ge [System.Version]"3.7.0"){
-    Set-Property -Name PYTHON_REQUIRED -Value 0
+    #Set-Property -Name PYTHON_REQUIRED -Value 0
+    $PYTHON_REQUIRED=$false
   }
   }
 }
 
-
+if($PYTHON_REQUIRED -eq $true)
+{
+    $true | Out-File $($($env:windir)+"\temp\PYTHON_REQUIRED.txt")
+}
+else{
+    echo "Not required"
+}
 
 
 
